a85d18c5d36dd35119f80729fa29267e8dafb783
Minh898989 <minhhh270805@gmail.com>
2025-05-16 19:07:08 +0700

    pỏt

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const token = process.env.TOKEN;
 const url = process.env.URL;
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 const app = express();
 app.use(express.json());
