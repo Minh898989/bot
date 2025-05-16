e1007d096c979767ca4a6d774bed0b0a0280f78d
Minh898989 <minhhh270805@gmail.com>
2025-05-16 18:19:32 +0700

    145

--- deleteWebhook.js
+++ deleteWebhook.js
@@ -1,12 +1,15 @@
 const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config();
 
-const token = '7653298046:AAHkkUmS3UJeVTEJ-Dpgi-uef4swNwEIQIw';
+const token = process.env.TOKEN;
 const bot = new TelegramBot(token);
 
 bot.deleteWebHook()
   .then(() => {
-    console.log("✅ Webhook đã được xóa thành công. Có thể chạy polling.");
+    console.log('✅ Webhook đã được xóa thành công');
+    process.exit(0);
   })
-  .catch((err) => {
-    console.error("❌ Lỗi khi xóa webhook:", err);
+  .catch(err => {
+    console.error('❌ Lỗi khi xóa webhook:', err);
+    process.exit(1);
   });
